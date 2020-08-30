b5c6014f7dbc70049328bcd0724817532e9a0507
Elmira Madadi <elmiramadadi@gmail.com>
2020-08-30 18:03:32 +0200

    Created OrderItemRepository.java + Tests

---
+++ src/main/java/se/lexicon/elmira/jpaworkshopordermanagement/data/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package se.lexicon.elmira.jpaworkshopordermanagement.data;
+
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.elmira.jpaworkshopordermanagement.entity.OrderItem;
+
+import java.util.List;
+
+public interface OrderItemRepository extends CrudRepository<OrderItem, Integer> {
+    List<OrderItem> findAll();
+}
---
+++ src/test/java/se/lexicon/elmira/jpaworkshopordermanagement/data/OrderItemRepositoryTest.java
@@ -0,0 +1,38 @@
+package se.lexicon.elmira.jpaworkshopordermanagement.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import se.lexicon.elmira.jpaworkshopordermanagement.entity.OrderItem;
+import se.lexicon.elmira.jpaworkshopordermanagement.entity.Product;
+import se.lexicon.elmira.jpaworkshopordermanagement.entity.ProductOrder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class OrderItemRepositoryTest {
+    OrderItem testObject;
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
+
+    @BeforeEach
+    void setUp() {
+        Product car = new Product( "Car", 1_000_000);
+        testObject = orderItemRepository.save(new OrderItem(2, car, null));
+    }
+
+    //Dose save() is working?
+
+    @Test
+    void SuccessfullyCreated() {
+        List<OrderItem> found = orderItemRepository.findAll();
+
+        assertFalse(found.isEmpty());
+        assertTrue(found.contains(testObject));
+    }
+}
